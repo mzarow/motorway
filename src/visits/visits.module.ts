876dbdaf49ddccf596f1a97f1d9f9dad697a6138
mzarow <mzarow@concisesoftware.com>
2022-06-25 20:57:32 +0200

    clean

--- src/visits/visits.module.ts
+++ src/visits/visits.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SharedModule } from '../shared/shared.module';
-import { ConfigModule } from '@nestjs/config';
 import { VisitsController } from './controllers/visits.controller';
 import { VisitsService } from './services/visits.service';
 
 @Module({
-  imports: [ConfigModule, SharedModule],
+  imports: [SharedModule],
   controllers: [VisitsController],
   providers: [VisitsService],
 })
